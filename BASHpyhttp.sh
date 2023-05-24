c7a39abd960249373bb84a643899fce022e5de91
Sam Scotson <118854225+Sam-Scotson@users.noreply.github.com>
2023-05-24 13:32:45 +1200

    Update BASHpyhttp.sh

--- BASHpyhttp.sh
+++ BASHpyhttp.sh
@@ -1,9 +1,19 @@
 #servers files from current working directory
 #BASH
  $ cd {desired directory to start server in}
- $ python3 -m http.server -b "::"(IPv6 binding){ip address} {port number (will default to 8000)} -d ~/{desired '/' directory} --cgi (CGI switch)
+ $ python3 -m http.server -b "::"(IPv6 binding){ip address} {port number (will default to 8000)} -d ~/{desired '/' directory} --cgi (CGI switch) 
 #create cgi-bin for CGI script function
  $ mkdir cgi-bin/
 
 #hosting cgi scripts
  $chmod +x cgi-bin/example.py
+
+#http password directory config
+ $ mkdir private/ public/
+
+ $ echo 'example1' > private/password1.txt
+ $ echo 'example2' > private/password2.txt
+ $ echo '<h1>TestWorking</h1>' > public/home.html
+
+ $ ln -s $(pwd)/private/password1.txt $(pwd)/public/linked_file.txt
+ $ ln -s $(pwd)/private $(pwd)/public/linked_dir
